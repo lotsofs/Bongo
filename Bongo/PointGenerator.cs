f00841a75ea45a73275b5270d8a0c0301f5af3ea
S <contact@lotsofs.com>
2019-04-01 02:29:57 +0200

    doc

--- Bongo/PointGenerator.cs
+++ Bongo/PointGenerator.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 
 namespace Bongo {
 	class PointGenerator {
-		// TODO: This assumes there's only 4 difficulty levels for goals. Make this flexible.
-		public static double Random(uint seed, uint scale, uint highestValue) {
+		/// <summary>
+		/// Generates random value between 0 and highestValue, uses algebraic functions to give a bias towards lower or higher numbers
+		/// </summary>
+		/// <param name="seed"></param>
+		/// <param name="bias">1 = add bias towards lower, 5 = higher. 0 = ignore</param>
+		/// <param name="highestValue"></param>
+		/// <returns></returns>
+		public static double Random(uint seed, uint bias, uint highestValue) {
 			Random rand = new Random((int)seed);
 			double num;
-			switch (scale) {
+			switch (bias) {
 				default: // disregarded
 					num = rand.Next(0, 100);
 					break;
-				case 1: // very easy
+				case 1: // very little
 					num = rand.Next(0, 100);
 					num = Math.Pow(num, 4);
 					num /= 4000000;
 					break;
-				case 2: // easy
+				case 2: // little
 					num = rand.Next(0, 100);
 					num = Math.Pow(num, 3);
 					num /= 15000;
@@ -29,12 +35,12 @@ namespace Bongo {
 					num = Math.Pow(num, 2);
 					num /= 110;
 					break;
-				case 4: // hard
+				case 4: // a lot
 					num = rand.Next(0, 100);
 					num = Math.Pow(num, 1.5);
 					num /= 10;
 					break;
-				case 5: // very hard
+				case 5: // a whole lot
 					num = rand.Next(0, 100);
 					num = Math.Pow(num, 0.5);
 					num /= 0.1;
